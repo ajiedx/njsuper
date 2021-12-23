23d4e43070f6b6cc2d0538ff76370b46429490e7
ajiedx <94229154+ajiedx@users.noreply.github.com>
2021-12-23 11:45:59 +0600

    +output, +pin, +isEnd, +filterChars, +splitOnce,
    rename compareBegining to isIntro

--- njsuper.js
+++ njsuper.js
@@ -5,7 +5,7 @@ class NjSuper {
             if (objx.hasOwnProperty('obj')) {
                 this.objx = objx.obj
                 delete objx.obj
-                
+
             } else if (objx.hasOwnProperty('objx')) {
                 if (objx.objx == false) {
                     this.objx = Object
@@ -16,7 +16,7 @@ class NjSuper {
             }
 
             Object.assign(this, objx)
-   
+
         }
 
         if (this.enum === true) {
@@ -26,7 +26,7 @@ class NjSuper {
                 writable: true,
                 configurable: true
             })
-            
+
         } else {
             this.dt = dt
         }
@@ -64,13 +64,19 @@ class NjSuper {
         }
     }
 
-    compareBegining(str, string) {
+    isEnd(str, string) {
+        const reg = new RegExp(str+'$', 's')
+        if (string.match(reg)) return true
+        else return false
+    }
+
+    isIntro(str, string) {
         string = string[Symbol.iterator]()
 
         for (const i in str) {
             if (str[i] !== string.next().value) {
                 return false
-            } 
+            }
         }
 
         return true
@@ -87,7 +93,7 @@ class NjSuper {
                 } else {
                     return new obj.obj(name, value)
                 }
-            } 
+            }
         } else if (obj.obj) {
             if (obj.obj instanceof Object) {
                 if (obj.objx) {
@@ -98,8 +104,8 @@ class NjSuper {
                 } else {
                     return new obj.obj(value, obj)
                 }
-               
-            } 
+
+            }
         } else if (this.objx) {
             return new this.objx(value, obj)
         } else {
@@ -146,7 +152,7 @@ class NjSuper {
         } else {
             return false
         }
-        
+
     }
 
     typeof(value) {
@@ -169,9 +175,112 @@ class NjSuper {
 
     valueInArray(value, arr) {
         for (const i in arr) {
-            if (this.compareBegining(arr[i], value)) return true
+            if (this.isIntro(arr[i], value)) return true
         }
         return false
+    }
+
+    filterChars(string, chars, rep) {
+        let newstring = ''
+        let char = false
+        for (let i = 0; i < string.length; i++) {
+            char = true
+            for (const l in chars) {
+                if (chars[l] === string[i]) char = false
+            }
+
+            if (char) newstring = newstring + string[i]
+            else if (rep) newstring = newstring + rep
+        }
+
+        return newstring
+    }
+
+
+
+    splitOnce(string, cut, right, last) {
+        let split = false, i = 0, l = 0, v = string.length - 1
+
+        let strings = {0: '', 1: ''}, cutlength = cut.length - 1, linelength = 1
+        if (last) {
+            for(;;) {
+                if (!split)
+                    i = Number(cutlength), l = 0
+                    for (let i in cut) {
+                        if (strings[1][i] === cut[i]) split = true
+                        else {
+                            split = false; break
+                        }
+                    }
+
+                if (split) {
+                    strings[0] = string[v] + strings[0]
+                } else {
+                    strings[1] = string[v] + strings[1]
+                }
+                if (v < 1) break
+                v = v - 1
+            }
+
+            if (!right) {
+                strings[0] = strings[0] + cut
+                const zeroclone = strings[1]
+                strings[1] = ''
+
+                for (let i in zeroclone) {
+                    if (cut[i] !== zeroclone[i]) strings[1] = strings[1] + zeroclone[i]
+                }
+            }
+        } else {
+            if (right) strings[1] = cut
+            for (let v in string) {
+                if (!split)
+                    i = cutlength, l = 2
+                    for (;;) {
+                        if (strings[0][linelength - l] === cut[i]) split = true
+                        else {
+                            split = false; break
+                        }
+                        if (i < 1) break
+                        i = i - 1, l = l + 1
+                    }
+
+                if (split) {
+                    strings[1] = strings[1] + string[v]
+                } else {
+                    strings[0] = strings[0] + string[v]
+                    linelength = linelength + 1
+                }
+            }
+
+
+
+            if (right) {
+                const zeroclone = strings[0]
+                strings[0] = ''
+                for (let s = 0; s < zeroclone.length - cutlength - 1; s++) {
+                    strings[0] = strings[0] + zeroclone[s]
+                }
+            }
+        }
+
+
+        return strings
+    }
+
+    splitOnceChars(string, chars) {
+        let array = ['', ''], split = false
+
+        for (let i in string) {
+            if (!split)
+                for (const l in chars) {
+                    if (chars[l] === string[i]) split = true
+                }
+
+            if (split) array[1] = array[1] + string[i]
+            else array[0] = array[0] + string[i]
+        }
+        return array
     }
 
     countChars(string, char) {
@@ -195,6 +304,27 @@ class NjSuper {
         }
     }
 
+    pin(obj, value, n) {
+        if (n) {
+            Object.assign(obj, {[n]: value, last: n})
+        } else {
+            let n = 0
+            for (let i in obj) {
+                if (!isNaN(i)) {
+                    if (n <= i) {
+                        i = Number(i)
+                        if (!obj.hasOwnProperty(i + 1)) {
+                            n = i + 1
+                        }
+                    }
+                }
+            }
+            Object.assign(obj, {[n]: value, len: n})
+        }
+
+
+    }
+
     assign(name, value, number) {
         if (name === 'this') {
             if (number) {
@@ -210,9 +340,9 @@ class NjSuper {
                 }
                 Object.assign(this, {[number]: value})
             } else if (this.typeof(value) === 'object') {
-            
+
                 Object.assign(this, value)
-                
+
             } else if (this.typeof(value) === 'string') {
                 let number = 0
                 for (const i in this) {
@@ -225,7 +355,7 @@ class NjSuper {
                     }
                 }
                 Object.assign(this, {[number]: value})
-    
+
             } else if (this.typeof(value) === 'array') {
                 if (this.has('array')) {
                     this.array.push(value)
@@ -237,10 +367,25 @@ class NjSuper {
                 Object.assign(this, {func})
             }
         } else {
-            if (this.typeof(value) === 'object') {
-            
+            if (number) {
+                let number = 0
+
+                for (const i in this[name]) {
+                    if (this.typeof(Number(i)) === 'number') {
+                        if(number <= Number(i)) {
+                            if(!this[name].hasOwnProperty(Number(i) + 1)) {
+                                number = Number(i) + 1
+                            }
+                        }
+                    }
+                }
+
+                Object.assign(this[name], {[number]: value})
+
+            } else if (this.typeof(value) === 'object') {
+
                 Object.assign(this[name], value)
-                
+
             } else if (this.typeof(value) === 'string') {
                 let number = 0
                 for (const i in this[name]) {
@@ -253,7 +398,7 @@ class NjSuper {
                     }
                 }
                 Object.assign(this[name], {[number]: value})
-    
+
             } else if (this.typeof(value) === 'array') {
                 if (this[name].hasOwnProperty('array')) {
                     this[name].array.push(value)
@@ -265,7 +410,7 @@ class NjSuper {
                 Object.assign(this[name], {func})
             }
         }
-        
+
     }
 
     add(value, name, objx) {
@@ -273,10 +418,9 @@ class NjSuper {
             this.check(value)
             this.assign('this', value)
         } else {
-
             this.check(name)
             if (this.has(name)) {
-                this.assign(name, value)    
+                this.assign(name, value)
             } else if (objx) {
                this.define(name, value, objx)
             } else {
@@ -294,7 +438,156 @@ class NjSuper {
     has(name) {
         return this.hasOwnProperty(name)
     }
-    
+
+    output(item, dp, endp) {
+        this.dps = 0
+        if (this.typeof(dp) === 'number') this.dps = dp
+        else if (this.typeof(endp) === 'number') this.dps = endp
+        const output = (item, depth=0, curly) => {
+            let tabs = '', theme = 'f', endstr = ''
+            let d9s0xcA = Date.now()
+            d9s0xcA = String(d9s0xcA)
+            let ssn = Number(d9s0xcA[d9s0xcA.length - 1])
+            let ccn = Number(d9s0xcA[d9s0xcA.length - 2])
+            let iin = Number(d9s0xcA[d9s0xcA.length - 3])
+            let colors = []
+            if (theme === 'f') {
+                colors = ['#f01a3a', '#1653fa', '#aa00ff', '#000000', '#000000',
+                '#f01a3a', '#1653fa', '#aa00ff', '#000000', '#02ccf0']
+            } else {
+                colors = ['#de3131', '#b06f15', '#e3dc0b', '#24e30b', '#0be3c6',
+                '#0b82e3', '#850be3', '#e30b9b', '#ffffff', '#ccc5b8']
+            }
+            let brc = colors[Number(d9s0xcA[d9s0xcA.length - 2])], idc = colors[Number(d9s0xcA[d9s0xcA.length - 3])]
+            let smc = colors[Number(d9s0xcA[d9s0xcA.length - 4])], stc = colors[Number(d9s0xcA[d9s0xcA.length - 5])]
+            let dnc = colors[Number(d9s0xcA[d9s0xcA.length - 6])]
+            brc = `color: ${brc};`, idc = `color: ${idc}; background-color: #eeeeee`, smc = `color: ${smc};`, stc = `color: ${stc};`
+            dnc = `color: ${dnc};`
+            let symbsign = ['|', ':', '^', '.', '+', '()', '{}', '[]', '*', '?']
+
+            for (var s = 0; s < depth -1; s++) {
+                if (symbsign[ssn].length === 1) tabs = tabs + '    ' + symbsign[ssn]
+                else tabs = tabs + '   ' + symbsign[ssn]
+
+            }
+
+            if (depth > 0) {
+                tabs = tabs + '    '
+            }
+
+            if (this.dps !== 0 && this.dps < depth) {
+
+                return
+            }
+
+            if (this.typeof(item) === 'object' || this.typeof(item) === 'array') {
+                let count = 0, scount = 0, objcount = 0, sh = []
+
+                for (let c in item) {
+                    count = count + 1
+                }
+                let obcount = count
+                if (count > 11) {
+                    let shc = 0
+                    for (var i = 0; i < 11; i++) {
+                        if (i < 5) sh.push(i)
+                        else sh.push(count - shc), shc = shc + 1
+                    }
+                }
+
+                for (let i in item) {
+                    if (!isNaN(i)) idc = dnc
+                    if (this.typeof(item[i])  === 'object' ) {
+                        obcount = obcount - 1
+                        if (this.dps !== 0 && this.dps - 1 < depth) {
+                            let obs = []
+                            for (let s in item[i]) {
+                                if (this.typeof(item[i][s]) === 'object') obs.push(s+': {}')
+                                else obs.push(s+': ...')
+                            }
+                            obs = obs.join(', ')
+                            console.log('%c' +  tabs  + '%c' + i + `%c:%c ${obs} `, brc, idc, smc, 'color: #fff; background-color: #3636')
+                        } else {
+                            console.log('%c' +  tabs  + '%c' + i + '%c: {', brc, idc, smc )
+                        }
+                        output(item[i], depth + 1, obcount)
+
+                    } else if (this.typeof(item) === 'array') {
+                        obcount = obcount - 1
+
+                        if (this.typeof(item[i]) === 'string' || this.typeof(item[i]) === 'number') {
+                            if (item[i].length === 0) {
+                                console.log('%c' +  tabs  + '%c' + i + `%c: ${item[i]}`+`%c|`, brc, idc, smc, stc)
+                            } else if (this.typeof(item[i]) === 'number') {
+                                console.log('%c' +  tabs  + '%c' + i + `%c = %c${item[i]}`, brc, idc, smc, stc)
+                            } else {
+                                console.log('%c' +  tabs  + '%c' + i + `%c: ${item[i]}`+`%c ${item[i].length}|`, brc, idc, smc, stc)
+                            }
+
+                        } else {
+
+                            if (i == 0) {
+                                console.log('%c' +  tabs  + '...%c' + i + `%c[%c${item[i].length}%c]`, brc, brc, smc, stc, smc)
+                            } else {
+                                console.log('%c' +  tabs  + '%c' + i + `%c[%c${item[i].length}%c]`, brc, idc, stc, smc, stc)
+                            }
+
+                            output(item[i], depth + 1, obcount)
+                        }
+
+                    } else {
+
+                        if (scount !== count - 1) {
+                            obcount = obcount - 1
+                            if (sh.length > 1) {
+                                for (let s in sh) {
+                                    if (scount === sh[s]) {
+                                        if (scount === 4) {
+                                            console.log('%c' + tabs + '%c' +  i + '%c: ' + `%c ${item[i]} `,
+                                                        brc, idc, smc, stc)
+                                            console.log('%c' + tabs + '%c' + '...', smc, brc)
+                                        } else {
+                                            console.log('%c' +tabs +'%c' +i + '%c: ' +  `%c ${item[i]} `,
+                                                        brc, idc, smc, stc)
+                                        }
+
+                                    }
+                                }
+                            } else {
+                                console.log('%c' + tabs + '%c' + i + '%c: ' +  `%c ${item[i]}`,
+                                            brc, idc, smc, stc)
+                            }
+                        } else {
+                            if (depth === 0) {
+                                if (this.typeof(dp) === 'string') endstr = dp
+                                else if (this.typeof(endp) === 'string') endstr = endp
+                            }
+                            obcount = obcount - 1
+                            let cur = ''
+                            if (curly > 0) {
+                                for (var cu = 0; cu < obcount + 1; cu++) {
+                                    cur = cur + ' }'
+                                }
+                                console.log('%c' + tabs + '%c' + i + '%c: '  +  `%c ${item[i]}`+ cur +`${endstr}`,
+                                            brc, idc, smc, stc)
+                            } else {
+                                console.log(`%c${tabs}` + `%c${i}` + '%c: ' + `%c ${item[i]}`+ `%c${tabs.slice(0, tabs.length - 6)}`+`%c } } ${endstr}` ,
+                                            brc, idc, smc, stc, brc, smc)
+                            }
+
+                        }
+                    }
+                    scount = scount + 1
+
+                }
+            } else {
+                console.log(item)
+            }
+        }
+
+        output(item)
+    }
+
 }
 
 module.exports = { NjSuper }
